6ef3af0a18f82dd1cba056c87f89c2f6c2aae9c6
Eyakhelil <eyakhelil485@gmail.com>
2025-05-23 23:32:44 +0100

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+# tasks/views.py
+from rest_framework.viewsets import ModelViewSet
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
